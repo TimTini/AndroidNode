d2ab67b95d9a611c85cbe23dce161dc643051ca3
timtini <timsb90@gmail.com>
2025-02-23 10:54:59 +0700

    refactor: simplify Brave desktop file update process in install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -6,17 +6,10 @@ install_brave() {
     curl -fsS https://dl.brave.com/install.sh | bash
 }
 
-# Function to find the Brave .desktop file
-find_brave_desktop_file() {
-    echo "Searching for Brave .desktop file..."
-    find /usr/share/applications ~/.local/share/applications -name "brave-browser.desktop" 2>/dev/null | head -n 1
-}
 
 # Function to update Brave .desktop file to add --no-sandbox
 update_brave_desktop() {
-    local desktop_file=$(find_brave_desktop_file)
-
-    sed -i 's|Exec=/usr/bin/brave-browser-stable %U|Exec=/usr/bin/brave-browser-stable --no-sandbox %U|' "$desktop_file"
+    sed -i 's|Exec=/usr/bin/brave-browser-stable %U|Exec=/usr/bin/brave-browser-stable --no-sandbox %U|' /usr/share/applications/brave-browser.desktop
     # Update the application database cache
     update-desktop-database ~/.local/share/applications/
 }
@@ -24,5 +17,3 @@ update_brave_desktop() {
 # Main execution flow
 install_brave
 update_brave_desktop
-
- sed -i 's|Exec=/usr/bin/brave-browser-stable %U|Exec=/usr/bin/brave-browser-stable --no-sandbox %U|' ~/.local/share/applications/brave-browser.desktop
