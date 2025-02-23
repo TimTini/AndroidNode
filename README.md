3b3650b062c71e37191996ae24eebc3e198c3f19
timtini <timsb90@gmail.com>
2025-02-23 08:14:53 +0700

    update installation scripts to ensure fresh Debian installation and improve script reliability

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 Chạy các lệnh sau trên Termux:
 
 ```sh
-curl -Lf https://raw.githubusercontent.com/TimTini/AndroidNode/refs/heads/main/install.sh -o install.sh && chmod +x install.sh && ./install.sh
+curl -Lf https://raw.githubusercontent.com/TimTini/AndroidNode/refs/heads/main/install.sh?$(date +%s) -o install.sh && chmod +x install.sh && ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Xóa Debian nếu đã tồn tại để đảm bảo cài đặt mới
-if proot-distro list | grep -q debian; then
-    echo "Đang xóa Debian cũ..."
-    proot-distro remove debian
-    rm -rf ~/.proot_debian  # Xóa thư mục dữ liệu cũ nếu có
-fi
-
 # Cập nhật & cài đặt các gói cần thiết trong Termux
 termux-change-repo
 pkg update -y && pkg upgrade -y
@@ -14,10 +7,15 @@ pkg install -y proot-distro pulseaudio x11-repo
 pkg install -y termux-x11-nightly
 
 # Cài đặt Debian
-proot-distro install debian
+if proot-distro list | grep -q debian; then
+    echo "Debian installed."
+else
+    proot-distro install debian
+fi
+
 
 # Tải script cài đặt Debian từ GitHub
-curl -Lf --no-cache --progress-bar -o "$HOME/debian-setup.sh" "https://raw.githubusercontent.com/TimTini/AndroidNode/main/debian-setup.sh?$(date +%s)"
+curl -Lf --progress-bar -o "$HOME/debian-setup.sh" "https://raw.githubusercontent.com/TimTini/AndroidNode/main/debian-setup.sh?$(date +%s)"
 chmod +x $HOME/debian-setup.sh
 
 # Copy file vào thư mục Debian
